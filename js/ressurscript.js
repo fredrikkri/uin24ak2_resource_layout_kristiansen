ee420fa8c0a65ee2ac581caba417c5fbfe97125a
fredrikkri <fjkri@hotmail.com>
2024-01-18 11:55:45 +0100

    nav bar funksjonen fungerer ikke enda
    lagt til endringer for å få opp ønsket innhold ved trykk i nav-bar.

--- js/ressurscript.js
+++ js/ressurscript.js
@@ -1,24 +1,46 @@
-let resourceHTML = ""
-let links = ""
-resources.map(resource => {
-    links = ""
-    resource.sources.map(source => links += `<a href="${source.url}>${source.title}</a>`)
-    resourceHTML +=
-    `<article>
-    <h3>${resource.category}</h3>
-    <p>${resource.text}</p>
-    <ul>
-        <li>
-            ${links}
-        </li>
-    </ul>
-    </article>`})
+function pickCategory(katName) {
+    let storedValue = katName
+    console.log("denne kjører")
+    showContent(storedValue)
+}
 
+// Navigasjonsbar
+let kategorierHTML = ""
+resources.map(kategori => {
+    kategorierHTML += `<li href="#" onclick="${pickCategory(kategori.category)}"><a>${kategori.category}</a></li>`
+})
 
-console.log(resourceHTML)
+const kategorier = document.getElementsByTagName("ul")
+kategorier[0].innerHTML = kategorierHTML
 
-const main = document.getElementsByTagName("main")
-main[0].innerHTML = resourceHTML
+// Main seksjon
+function showContent(storedValue){
+    let resourceHTML = ""
+    let links = ""
+
+    resources.map(resource => {
+        if(storedValue === resource.category) {
+            links = ""
+            resource.sources.map(source => links += `<a href="${source.url}">${source.title}</a>`)
+            resourceHTML +=
+            `<article>
+            <h3>${resource.category}</h3>
+            <p>${resource.text}</p>
+            <ul>
+                <li>
+                    ${links}
+                </li>
+            </ul>
+            </article>`}
+        else {
+            `<article>no content</article>`
+        }})
+        
+    console.log(resourceHTML)
+
+    const main = document.getElementsByTagName("main")
+    main[0].innerHTML = resourceHTML
+}
 
 // Her har jeg latt meg inspirere av javascript-kode
 // som ble brukt til å kode legodudes-prosjektet.
